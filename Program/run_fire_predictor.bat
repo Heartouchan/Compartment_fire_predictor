8a0fd38d53b76db31345e7882470f15b5a181904
Heartouchan <163482184+Heartouchan@users.noreply.github.com>
2025-06-12 17:00:54 -0400

    Add files via upload

---
+++ Program/run_fire_predictor.bat
@@ -0,0 +1,20 @@
+@echo off
+title Fire Curve Predictor Launcher
+echo Checking Python environment...
+
+
+REM 切换到当前目录
+cd /d %~dp0
+
+echo Installing required packages (if not already installed)...
+pip install -r requirements.txt
+
+echo.
+echo Running Program.py...
+python Program.py
+
+echo.
+echo ------------------------------
+echo   Program finished running.
+echo   Press any key to exit...
+pause > nul
